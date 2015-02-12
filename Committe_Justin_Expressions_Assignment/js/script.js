ba0d6d4c56c931093cd43843253d81a98bebd9a4
jocommitte <jocommitte.com>
2015-02-11 21:24:38 -0500

    Expressions Assignment Two sides

--- Committe_Justin_Expressions_Assignment/js/script.js
+++ Committe_Justin_Expressions_Assignment/js/script.js
@@ -4,5 +4,7 @@
  Expressions Assignment
  */
 
-var length = prompt("Length", 12+" ft");
-var width = prompt("Width", 12 +" ft");
+var side1 = prompt("Short Side");
+var side2 = prompt("Middle Side");
+var side3 = Math.pow(side1,2) + Math.pow(side2,2);
+var final; 
