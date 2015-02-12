47fc6ba38b6edabc28ab89a2d25525f43eee92ec
jocommitte <jocommitte.com>
2015-02-11 21:49:41 -0500

    Answer in HTML
    the answer is displayed in the HTML

--- Committe_Justin_Expressions_Assignment/js/script.js
+++ Committe_Justin_Expressions_Assignment/js/script.js
@@ -7,4 +7,7 @@
 var side1 = prompt("Short Side");
 var side2 = prompt("Middle Side");
 var side3 = Math.pow(side1,2) + Math.pow(side2,2);
-var final; 
+var final = Math.sqrt(side3);
+alert(final);
+document.getElementById("p1").innerHTML = final + " is the length of your long side";
+
