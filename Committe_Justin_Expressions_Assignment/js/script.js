453c76ac06f67a4308c02a28ecd8d5fd65a88660
jocommitte <jocommitte.com>
2015-02-11 22:25:12 -0500

    Using a function to round

--- Committe_Justin_Expressions_Assignment/js/script.js
+++ Committe_Justin_Expressions_Assignment/js/script.js
@@ -8,7 +8,10 @@ var side1 = prompt("Short Side");
 var side2 = prompt("Middle Side");
 var side3 = Math.pow(side1,2) + Math.pow(side2,2);
 var hello = Math.sqrt(side3);
-var final = Math.ceil(hello*100)/100;
+var final = money_round(hello);
+function money_round(hello) {
+    return Math.ceil(hello * 100) / 100;
+}
 console.log(final)
 
 document.getElementById("p1").innerHTML = "Congratulations "+ final + " is the length of your long side";
