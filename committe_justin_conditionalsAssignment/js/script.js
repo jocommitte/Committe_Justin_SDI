c4e75332f9786cb146600105e5648331c81eb5e3
jocommitte <jocommitte.com>
2015-02-19 21:23:32 -0500

    fixed average

--- committe_justin_conditionalsAssignment/js/script.js
+++ committe_justin_conditionalsAssignment/js/script.js
@@ -8,26 +8,27 @@
 //find out if he is passing or not. 70 or above is passing.
 //if he is passing and he has at least one A then he is eligible for AP Classes
 
-var name = prompt('What is your name?');
-var math = prompt("What grade did you get in Math Class?");
-if(math == ''){
+var name = prompt('What is your name?');//user input for name
+var math = prompt("What grade did you get in Math Class?");//user input for math grade
+if(math == ''){//if statement validates for if the information was not inputted
     prompt("Please enter your grade for math.");
 }
-var english;
-english = prompt('What grade did you get in English Class?');
-if(english == ''){
-    prompt("Please enter a grade for english.");
+var english = prompt('What grade did you get in English Class?');//user input for English grade
+if(english == ''){//if there is an empty string then run this code
+    prompt("Please enter a grade for english.");//if no input then tells user to write the grade
 }
-var science = prompt("What grade did you get in Science Class?");
-if(science == ''){
+var science = prompt("What grade did you get in Science Class?");////user input for science class
+if(science == ''){//if input is empty string then write the input
     prompt("Please enter a grade for science.");
 }
-math = parseInt(math);
+math = parseInt(math);//turning the three strings below into numbers
 english = parseInt(english);
 science = parseInt(science);
-var oneA;
-var average = math + english + science;
-passing = average >= 70 ? "you passed":"you failed";
+var oneA;//initializing the oneA variable used to see if the user has atleast one a
+var avg = (math + english + science)/3;//getting
+console.log(avg);
+var passing;
+passing = avg >= 70 ? "you passed":"you failed";
 if(passing == "you failed"){
     console.log('You Failed');
 }else{
@@ -43,6 +44,7 @@ if(passing == "you failed"){
         console.log("Good Job "+ name +" ,you passed, but you are not eligible for AP classes!")
     }
 }
+
 
 
 
