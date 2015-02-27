19d965c2f96242b982f0c77c8a5ccd7af6837b57
jocommitte <jocommitte.com>
2015-02-26 22:50:56 -0500

    found a way to make none of them duplicates

--- Committe_Justin_Functions_Assignmenty/js/script.js
+++ Committe_Justin_Functions_Assignmenty/js/script.js
@@ -10,21 +10,45 @@ while(kind == ''){
     kind= prompt('Please enter a 6 for florida lottery or a 5 for powerball');
 }
 if(kind == 5){
-    amount = 59
+    amount = 59;
 }else{
     amount = 53;
 }
-
+function powerBall(){
+    var pb = Math.floor(Math.random()*35);
+    return(pb);
+}
+//function lottery(kin,amt) {
+//    var balls = [];
+//    for (var i=1; i<=kin; i++) {
+//
+//        var lottery = Math.floor(Math.random() * amt);
+//        balls.push(lottery)
+//
+//
+//    }
+//    return(balls);
+//
+//}
 function lottery(kin,amt) {
     var balls = [];
-    for (var i=1; i<=kin; i++) {
 
-        var lottery = Math.floor(Math.random() * amt);
-        balls.push(lottery)
-
-
+    for (var i = 1; i < amt; i++) {
+        balls.push(i);
     }
-    return(balls);
+
+    while (balls.length > kin) {
+        balls.splice(Math.floor(Math.random() * balls.length), 1);
+    }
+
+    return balls;
+};
+if(kind == 5){
+    console.log("Here are your Lottery Numbers "+lottery(kind,amount)+" and here is your powerball number: "+ powerBall()+" Yaaay!");
+
+}else{
+    console.log("Here are your Florida Lottery Numbers "+lottery(kind,amount)+"!!");
 
 }
-console.log(lottery(kind,amount));
+
+
